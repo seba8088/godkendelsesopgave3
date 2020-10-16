28027c10ef9583664123323cbfad801729dec14f
Sebastian <seba8088@outlook.com>
2020-10-16 20:02:27 +0200

    commiting last classes and controllers folder

---
+++ Models/image.js
@@ -0,0 +1,9 @@
+//defining a class for images
+class Image {
+    constructor(uploadDate, fileName, fileSize, fileURL) {
+        this.uploadDate = uploadDate;
+        this.fileName = fileName;
+        this.fileSize = fileSize;
+        this.fileURL = fileURL;
+    }
+}
---
+++ Models/match.js
@@ -0,0 +1,8 @@
+//defining a class for matches
+//user1 and user2 corresponds to two users who have 'liked' each other
+class Match {
+    constructor(user1, user2) {
+    this.user1 = user1;
+    this.user2 = user2;
+    }
+}
